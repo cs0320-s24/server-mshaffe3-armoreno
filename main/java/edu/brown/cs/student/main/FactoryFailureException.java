17c16f0e8bd201e3c91f4036603442392bcb34e5
astrid200408 <astridsanabria4@gmail.com>
2024-02-10 15:32:55 -0500

    feat: added csv stuff and pom.xml

---
+++ main/java/edu/brown/cs/student/main/FactoryFailureException.java
@@ -0,0 +1,24 @@
+package edu.brown.cs.student.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This is an error provided to catch any error that may occur when you create an object from a row.
+ * Feel free to expand or supplement or use it for other purposes.
+ */
+public class FactoryFailureException extends Exception {
+  final List<String> row;
+
+  /**
+   * Exception that is thrown when a malformed row is encountered
+   *
+   * @param message - message that is printed
+   * @param row - malformed row that is printed
+   */
+  public FactoryFailureException(String message, List<String> row) {
+    super(message);
+    this.row = new ArrayList<>(row);
+    System.out.println("This row is malformed: " + this.row);
+  }
+}
---
+++ main/java/edu/brown/cs/student/main/ValueNotFoundException.java
@@ -0,0 +1,7 @@
+package edu.brown.cs.student.main;
+
+public class ValueNotFoundException extends Exception {
+  public ValueNotFoundException(String message) {
+    super(message);
+  }
+}
